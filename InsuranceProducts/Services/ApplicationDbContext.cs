1f33da4edf72e5e51b6b56c94c45ffed471d4451
Vaujany Kamdem Domguia <kava2211@student.iepscf-uccle.be>
2024-03-23 05:27:52 +0100

    Ajoutez des fichiers projet.

---
+++ InsuranceProducts/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InsuranceProducts.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceProducts.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
